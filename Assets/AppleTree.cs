cf4371aaa880259cfe45c5ff420e967afff3ea77
Matthew Weil <matt@weil.dev>
2020-09-18 19:02:15 -0400

    add tree movement + direction

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+    // Prefab for instantiating apples
+    public GameObject   applePrefab;
+
+    // Speed at which the AppleTree moves in meters/second
+    public float        speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float        leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float        chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float        secondsBetweenAppleDrops = 1f;
+
+    void Start () {
+        // Dropping apples every second
+    }
+
+    void Update () {
+        // Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		pos.x += 1.0f * 0.01f;
+		pos.x += 0.01f;
+		transform.position = pos;
+        // Changing Direction
+		if ( pos.x < -leftAndRightEdge ) {
+			speed = Mathf.Abs(speed);  // Move right
+		} else if ( pos.x > leftAndRightEdge ) {
+			speed = -Mathf.Abs(speed); // Move left
+		} 
+    }
+}
