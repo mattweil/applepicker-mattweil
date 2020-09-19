80c2d1442b58de3a026baffa445d5f5bdbfe5bab
Matthew Weil <matt@weil.dev>
2020-09-19 15:23:49 -0400

    multiple baskets should now appear

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;  
 
 public class ApplePicker : MonoBehaviour {
 
@@ -7,13 +8,16 @@ public class ApplePicker : MonoBehaviour {
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     void Start () {
-        for (int i=0; i<numBaskets; i++) {
+        basketList = new List<GameObject>();
+        for (int i=0; i< numBaskets; i++) {
             GameObject tBasketGO = Instantiate( basketPrefab ) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + ( basketSpacingY * i );
             tBasketGO.transform.position = pos;
+            basketList.Add( tBasketGO );
         }
     }
     public void AppleDestroyed() {                                          // 2
@@ -22,5 +26,17 @@ public class ApplePicker : MonoBehaviour {
         foreach ( GameObject tGO in tAppleArray ) {
             Destroy( tGO );
         }
+        //// Destroy one of the Baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count-1;
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        // Remove the Basket from the List and destroy the GameObject
+        basketList.RemoveAt( basketIndex );
+        Destroy( tBasketGO );
+        if ( basketList.Count == 0 ) {
+            Application.LoadLevel( "Main" );
+        }
     }
+
 }
