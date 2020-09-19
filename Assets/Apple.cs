2912b437d088087a1242fbcfe7bcb2388f49b71f
Matthew Weil <matt@weil.dev>
2020-09-18 20:41:12 -0400

    create apple script

---
+++ Assets/Apple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    public static float bottomY = -20f;
+
+    void Update () {
+        if ( transform.position.y < bottomY ) {
+            Destroy( this.gameObject );
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+
+}
